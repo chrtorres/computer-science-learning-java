60c927e373217d533ab2ff642975bd3ea40e8a5f
Christopher Torres <chris.rtorres@outlook.com>
2023-09-04 12:16:04 -0400

    Finished code in section3 for Deleting a node from list

--- code/section3/DeleteFromList.java
+++ code/section3/DeleteFromList.java
@@ -4,23 +4,23 @@ package section3;
  * Here we will implement our own SinglyLinkedList from scratch within this class.
  * Once the logic is created we can handle insertion, deletion, and resizing of the list.
  * 
- * ***** Code Still In Progress *********
  * 
  * 
  */
 public class DeleteFromList {
 
     public static class LinkedList<E> {
-    
+
         public static class Node<E> {
             private E element;
             private Node<E> next;
 
             // constructor
-            public Node(E e, Node<E> n){
+            public Node(E e, Node<E> n) {
                 element = e;
                 next = n;
             }
+
             // access the element of a node
             public E getElement() {
                 return element;
@@ -43,8 +43,9 @@ public class DeleteFromList {
         private int size = 0;
 
         // constructor for an empty list
-        public LinkedList(){}
-        
+        public LinkedList() {
+        }
+
         // access methods
         public int size() {
             return size;
@@ -67,26 +68,83 @@ public class DeleteFromList {
                 return null;
             return tail.getElement();
         }
-        
-        public Node<E> getHead(){
+
+        public Node<E> getHead() {
             return head;
         }
 
         // update methods
-        public void addFirst(E e){
+        public void addFirst(E e) {
             head = new Node<>(e, head);
-            if (size == 0)  // if list is empty then head = tail
+            if (size == 0)              // if list is empty then head = tail
                 tail = head;
-            size++; // increase size of list after insertion
+            size++;                     // increase size of list after insertion
         }
 
-        public void addLast(E e){
+        public void addLast(E e) {      // add element at end of list
             Node<E> newest = new Node<>(e, null);
+            if (isEmpty())
+                head = newest;          // if list is empty, set head = to inserted node
+            else
+                tail.setNext(newest);   // new node after existing tail
+            tail = newest;              // if list not empty, make tail = inserted node
+            size++;
         }
 
+        public E removeFirst() {
+            if (isEmpty())
+                return null;
+            E answer = head.getElement();
+            head = head.getNext();
+            size--;
+            if (size == 0)
+                tail = null;
+            return answer;
+        }
+
+        public void traverse() {
+            Node<E> current = head;
+
+            while (current != null) {
+                System.out.print(current.element + " -> ");
+                current = current.next;
+            }
+            System.out.print("null\n");
+        }
+
+        public void deleteNode(E data) {
+            Node<E> currentNode = head;
+            Node<E> prevNode = null;
+
+            while (currentNode != null) {       // check if empty or if we reached end of list
+                if (currentNode.element == data) {
+                    if (prevNode == null)
+                        head = currentNode.next;// if node to be deleted is head, set head = next
+
+                    else
+                        prevNode.next = currentNode.next;   // if not head, point to next node
+                    size--;                     // adjust size of list
+                }
+                prevNode = currentNode;         // move pointers to traverse the list
+                currentNode = currentNode.next;
+            }
+        }
     }
 
     public static void main(String args[]) {
 
+        int[] data = { 3, 6, 1, 6, 10, 6 };
+        LinkedList<Integer> list = new LinkedList<>(); // create empty list
+
+        for (int i : data)                      // insert data into list
+            list.addLast(i);
+
+        list.traverse();                        // traverse the list to verify insertion
+        System.out.println("Size of List before deletion: " + list.size());
+
+        list.deleteNode(6);
+
+        list.traverse();
+        System.out.println("Size of List after deletion: " + list.size());
     }
 }
